651d94b2dae41a832529f6285b2bb67f18ed2ea5
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-03-13 22:44:02 +0200

    Add logic to change page, add functions to create Main & Menu page + clear elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import {createMain} from './mainPage';
+import {createMenu} from './menu';
 
 console.log('load header');
 
 const divContent = document.querySelector('#content');
-//createMain.backgroundImg();
-let header = createMain.headerElements();
-divContent.appendChild(header);
 
+const header = createMain.headerElements();
+//Create main page as default
+divContent.className = "home";
+createMainPage();
 
-console.log('load main');
+// Choosing which page to display
+document.querySelectorAll('button').forEach(tabs => {
+    tabs.addEventListener('click', event => {
+        console.log(event.target.id);
+        if (event.target.id === "homeButton") {
+            divContent.className = "home";
+            console.log("Change to home");
+            clearContent();
+            createMainPage();
+        } else if (event.target.id === "menuButton") {
+            divContent.className = "menu";
+            console.log("Change to menu");
+            clearContent();
+            createMenuPage();
+        } else {
+            //divContent.className = "contacts";
+            console.log("Change to contacts");
+        }
+    });
+});
 
-createMain.headlineElement();
+// displaying the main page
+function createMainPage() {
+    divContent.appendChild(header);
+    createMain.headlineElement();
+    createMain.pageFooter();
+}
 
-createMain.pageFooter();
+//displaying the menu
+function createMenuPage() {
+    divContent.appendChild(header);
+    const menu = createMenu.menuItem();
+    divContent.appendChild(menu);
+    const menuFood = createMenu.foodMenu();
+    divContent.appendChild(menuFood);
+    const menuDrinks = createMenu.drinksMenu();
+    divContent.appendChild(menuDrinks);
+    createMain.pageFooter();
+}
 
-console.log("end");
+// clearing all of the elements
+function clearContent() {
+    while (divContent.firstChild) {
+        divContent.firstChild.remove();
+    }
+}
